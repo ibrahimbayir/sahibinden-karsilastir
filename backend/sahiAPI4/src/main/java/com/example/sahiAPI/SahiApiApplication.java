242faac2592e4fa9904f73e58506328a35c35896
İbrahim Bayır <94011558+ibrahimbayir@users.noreply.github.com>
2025-06-03 12:23:08 +0300

    Update SahiApiApplication.java

--- backend/sahiAPI4/src/main/java/com/example/sahiAPI/SahiApiApplication.java
+++ backend/sahiAPI4/src/main/java/com/example/sahiAPI/SahiApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.example.sahiAPI")
 public class SahiApiApplication {
 
 	public static void main(String[] args) {
